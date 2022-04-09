92a5a695890ae54c7f34d11347125d2fa3c9932c
Asphyxia <stefanvesovic2@gmail.com>
2022-04-09 21:53:01 +0200

    chaning some stuff up

--- src/main.c
+++ src/main.c
@@ -4,19 +4,17 @@
 #include <string.h>
 
 #include "inputBuffer.h"
+#include "metaCommand.h"
+#include "statement.h"
 
 int main() {
-    InputBuffer* input_buffer = new_input_buffer();
+    struct InputBuffer* input_buffer = new_input_buffer();
 
     while (true) {
         print_prompt();
         read_input(input_buffer);
 
-        if (strcmp(input_buffer->buffer, ".exit") == 0) {
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
-        } else {
-            printf("Unrecognized command '%s'. \n", input_buffer->buffer);
+        if (strcmp(input_buffer, ".exit") == 0) {
         }
     }
 }
