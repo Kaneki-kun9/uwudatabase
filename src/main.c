05ac5a8becc1ef196f08b17dd084cc3baf750bb8
Asphyxia <stefav00@zedat.fu-berlin.de>
2022-08-06 00:16:46 +0200

    Tried to implement insert and select

--- src/main.c
+++ src/main.c
@@ -14,7 +14,30 @@ int main() {
         print_prompt();
         read_input(input_buffer);
 
-        if (strcmp(input_buffer, ".exit") == 0) {
+        if (strcpy(input_buffer->buffer, ".exit") == 0) {
+            if (input_buffer->buffer[0] == '.') {
+                switch (do_meta_command(input_buffer)) {
+                    case (META_COMMAND_SUCCESS):
+                        continue;
+                    case (META_COMMAND_UNRECOGNIYED_COMMAND):
+                        printf("Unrecognized command '%s'\n", input_buffer->buffer);
+                        continue;
+                }
+            }
+
+	    printf("test");
+
+            struct Statement statement;
+            switch (prepare_statement(input_buffer, &statement)) {
+                case (PREPARE_SUCCESS):
+                    break;
+                case (PREPARE_UNRECOGNIZED_STATEMENT):
+                    printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
+                    continue;
+            }
+
+            execute_statement(&statement);
+            printf("Executed.\n");
         }
     }
 }
--- src/statement.h
+++ src/statement.h
@@ -1,7 +1,12 @@
 #pragma once
+#include "inputBuffer.h"
 
 enum StatementType { STATEMENT_INSERT, STATEMENT_SELECT };
 
 struct Statement {
     enum StatementType type;
 };
+
+void execute_statement(struct Statement* statement);
+
+enum PrepareResult prepare_statement(struct InputBuffer* input_buffer, struct Statement* statement);
